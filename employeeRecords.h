2563783f317de81e5e105965d19f2d39df55fa9a
IIconn <32812415+IIconn@users.noreply.github.com>
2017-10-18 00:33:08 +0800

    Employee record create, update and delete
    with duplicate name and minor age error trapping

---
+++ employeeRecords.h
@@ -0,0 +1,25 @@
+#ifndef __EMPLOYEERECORDS_H_INCLUDED__
+#define __EMPLOYEERECORDS_H_INCLUDED__
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+class employeeRecords{
+	private:
+		int employeeID, age; ;																							//employee info(integers)
+		string contactNumber, name, address, emailAddress, birthday, sex, maritalStatus, startDate, employmentStatus;	//employee info(strings)
+		string fathersName, fathersOccupation, mothersName, mothersOccupation;											//parents info
+		string educationalAttainment, schoolName, schoolAddress;														//educational attainment info(string)
+		int yearGraduated,baseSalary;	
+		
+		ifstream erInFile;
+		ofstream erOutFile;				
+	public:		
+		bool searchEmployee(string,string);
+		bool createNewEmployee();
+		bool deleteEmployee();
+		bool updateEmployee();
+		bool rewriteEmployeeRecord();
+};
+
+#endif
